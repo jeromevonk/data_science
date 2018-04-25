67f8f5ff3afeab28841d0d2a5f930d3448f31682
Jerome Vonk <jeromevonk@users.noreply.github.com>
2018-04-25 01:03:50 -0300

    added results of queries to mongodb

---
+++ Wrangling OSM data for Madrid/3. Preparing_database/schema_mongodb.py
@@ -0,0 +1,65 @@
+"""
+{
+"id": "2406124091",
+"type: "node",
+"visible":"true",
+"created": {
+          "version":"2",
+          "changeset":"17206049",
+          "timestamp":"2013-08-03T16:43:42Z",
+          "user":"linuxUser16",
+          "uid":"1219059"
+        },
+"pos": [41.9757030, -87.6921867],
+"address": {
+          "housenumber": "5157",
+          "postcode": "60625",
+          "street": "North Lincoln Ave"
+        },
+"amenity": "restaurant",
+"cuisine": "mexican",
+"name": "La Cabana De Don Luis",
+"phone": "1 (773)-271-5176"
+}
+
+
+- you should process only 2 types of top level tags: "node" and "way"
+- all attributes of "node" and "way" should be turned into regular key/value pairs, except:
+    - attributes in the CREATED array should be added under a key "created"
+    - attributes for latitude and longitude should be added to a "pos" array,
+      for use in geospacial indexing. Make sure the values inside "pos" array are floats
+      and not strings. 
+- if the second level tag "k" value contains problematic characters, it should be ignored
+- if the second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
+- if the second level tag "k" value does not start with "addr:", but contains ":", you can
+  process it in a way that you feel is best. For example, you might split it into a two-level
+  dictionary like with "addr:", or otherwise convert the ":" to create a valid key.
+- if there is a second ":" that separates the type/direction of a street,
+  the tag should be ignored, for example:
+
+<tag k="addr:housenumber" v="5158"/>
+<tag k="addr:street" v="North Lincoln Avenue"/>
+<tag k="addr:street:name" v="Lincoln"/>
+<tag k="addr:street:prefix" v="North"/>
+<tag k="addr:street:type" v="Avenue"/>
+<tag k="amenity" v="pharmacy"/>
+
+  should be turned into:
+
+{...
+"address": {
+    "housenumber": 5158,
+    "street": "North Lincoln Avenue"
+}
+"amenity": "pharmacy",
+...
+}
+
+- for "way" specifically:
+
+  <nd ref="305896090"/>
+  <nd ref="1719825889"/>
+
+should be turned into
+"node_refs": ["305896090", "1719825889"]
+"""
